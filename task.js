a4659d38ec060925e403e2a081ab833f2e1cdb62
Leonid Sharlandzhiev <luvche@gmail.com>
2023-01-24 23:28:55 +0200

    final answer

--- task.js
+++ task.js
@@ -1,31 +1,38 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
-//TASK: Count how many times the word BANANA can be written within a string S
-
-let stillSatisfyCondition = false;
+let counter = 0;
 
 function solution(S) {
     // Implement your solution here
-    console.log(`string ${S} length ${S.length}`);
-    // hello
-    let searchFor = ["B", "N", "A"];
 
-    for (let i = 1; i < 4; i++) {
-        for (j = 0; j < i; j++) {
-            S = S.replace(searchFor[i - 1], "");
+    console.log(`Iteration ${counter + 1}\n - OLD string [${S}] length (${S.length})`);
+
+    // Search for 3xA, 2xN, 1xB in the remaining string
+    const searchPattern = new RegExp('^(?=.*A.*A.*A)(?=.*N.*N)(?=.*B).*$', 'g');
+    const isAllLettersPresent = searchPattern.test(S);
+
+    // End loop when not all letters are preent in the string
+    if (isAllLettersPresent === false) {
+        console.log(`\n(i) No more matches found`);
+        return
+    }
+
+    let lettersPool = ["B", "N", "A"];
+
+    for (let letterIndex = 0; letterIndex < lettersPool.length; letterIndex++) {
+        for (repeatCount = 0; repeatCount < letterIndex + 1; repeatCount++) {
+            S = S.replace(lettersPool[letterIndex], "");
         }
     }
 
+    counter++;
+    console.log(` - NEW string [${S}] length (${S.length})\n\n`);
 
-
-    console.log(`string ${S} length ${S.length}`);
-
-    for (let letter of searchFor) {
-        stillSatisfyCondition = stillSatisfyConditionS.test();
-    }
-
-    if (stillSatisfyCondition === true) {
-        solution(S);
-    }
+    solution(S);
 }
+
+// solution("AAKBOKAOKNVJNA9EYRUSI;VBNXBVA=SEUEIASTGBVHXJAHRFTONFLBALYRA[U");
+// solution("AAANBN-AAANBN-AAANBN-AAANBN-AAANBN-AAANBN-AAANBN-AAAXBN");
+// solution("ABC");
+console.log(`\n\n\nFINAL ANSWER\n - 3xA, 2xN, 1xB was found (${counter}) times in the pattern\n\n\n`);
